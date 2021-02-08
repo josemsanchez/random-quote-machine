673983e7e7ff2e895bbd9b54b544b41aaebd949a
Jose Sanchez <jmsanchezdiaz@gmail.com>
2021-02-08 18:42:56 +0000

    Add random quote machine

---
+++ quotes.js
@@ -0,0 +1,22 @@
+const QUOTESBANK=[
+			{
+				quote:"Learn balance Daniel San... Wax-on... Wax-off",
+				author: "Miyagi"
+			},
+			{
+				quote:"No such thing as bad student,only bad teacher.",
+				author: "Miyagi"
+			},
+			{
+				quote:" We do not train to be merciful here. Mercy is for the weak. Here, in the streets, in competition: A man confronts you, he is the enemy. An enemy deserves no mercy.",
+				author: "Kreese"
+			},
+			{
+				quote:"It doesn't matter if you're a loser, or a nerd, or a freak! All that matters is that you become badass!",
+				author:"Johnny Lawrence"
+			},
+			{
+				quote:"Strike first, no mercy? Real good lessons. If you think I'm gonna let you fill these kids heads with that garbage, you're nuts, man.",
+				author:"Daniel LaRusso"
+			}
+		];
